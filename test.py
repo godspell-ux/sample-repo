a0d0e3cc3ad75e0ab3a1b737ff3427b863c8b1a9
James Madriaga <jamesmadriaga120@gmail.com>
2026-01-15 20:52:47 +0800

    added sample operations

--- test.py
+++ test.py
@@ -8,3 +8,23 @@ elif name == "Madriaga":
    print("You typed your last name.")
 else:
    print("You typed neither your first nor last name.")
+
+#LIST OPERATIONS
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+print(fruits)
+
+#TUPLE OPERATIONS
+coordinates = (10.0, 20.0)
+print("X Coordinate:", coordinates[0])
+print("Y Coordinate:", coordinates[1])
+
+#SET OPERATIONS
+unique_numbers = {1, 2, 3, 4, 5}
+unique_numbers.add(6)
+print(unique_numbers)
+
+#DICTIONARY OPERATIONS
+person = {"name": "James", "age": 21}
+person["city"] = "PH"
+print(person)
