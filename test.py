b8fa5b76bd759b1e6e3e418f084a2e9c11f488c6
James Madriaga <jamesmadriaga120@gmail.com>
2026-01-15 20:37:06 +0800

    first input program

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
 print("Hello World!!!")
+
+name = input("Enter your name: ")
+
+if name == "James":
+    print("You typed your first name.")
+elif name == "Madriaga":
+   print("You typed your last name.")
+else:
+   print("You typed neither your first nor last name.")
